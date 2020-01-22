f108c174d6af5db21448e013b9f492f05c1b365d
iggy-pigy <ilgakokorisa@gmail.com>
2020-01-22 19:19:08 +0000

    Added function for POST endpoint

--- tasks.js
+++ tasks.js
@@ -7,13 +7,23 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/tasks", function(req, res) {
+app.get("/tasks", function(request, response) {
+  console.log(request);
+
   response.status(200).json({
     tasks: [
       { text: "water plants", completed: true, id: 1 },
       { text: "buy milk", completed: true, id: 2 },
       { text: "call mom", completed: false, id: 3 }
     ]
+  });
+});
+
+app.post("/tasks", function(req, res) {
+  const text = req.body.text;
+
+  res.json({
+    message: `Received a request to add task ${text}`
   });
 });
 
